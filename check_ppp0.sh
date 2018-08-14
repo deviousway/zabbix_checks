245b2b116cc379423595734cf7ee38c01e2c9f60
deviousway <3791326+deviousway@users.noreply.github.com>
2018-08-14 20:14:58 +0300

    Update check_ppp0.sh

--- check_ppp0.sh
+++ check_ppp0.sh
@@ -3,8 +3,8 @@ if [ ! -f /sys/class/net/ppp0/carrier ]; then
     echo 1
         else 
         link_status=$(cat /sys/class/net/ppp0/carrier)
-        if  [ $link_status="1" ]; 
-        then echo 0
+            if  [ $link_status="1" ]; 
+            then echo 0
         else
         echo 1
         fi
